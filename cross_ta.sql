a2584abbc5b6bc2c2415120657f766e443f066ba
isaacmg <igodfried@isaac26.com>
2023-04-27 02:00:44 -0300

    r e 4r 1

---
+++ cross_ta.sql
@@ -0,0 +1,14 @@
+create table  mimic_iv.extra_vars as
+  select * from crosstab(
+    'select ch.stay_id, d.label, avg(valuenum) as value
+      from chartevents as ch
+        inner join mimic_iv.icustays as i
+          on ch.stay_id = i.stay_id
+        inner join d_items as d
+          on d.itemid = ch.itemid
+        where ch.valuenum is not null
+          and d.label in (''Admission Weight (Kg)'', ''GCS - Eye Opening'', ''GCS - Motor Response'', ''GCS - Verbal Response'', ''Height (cm)'')
+          and ch.valuenum != 0
+          and date_part(''hour'', ch.charttime) - date_part(''hour'', i.intime) between -24 and 5
+        group by ch.stay_id, d.label'
+        ) as ct(stay_id integer, weight double precision, eyes double precision, motor double precision, verbal double precision, height double precision);
