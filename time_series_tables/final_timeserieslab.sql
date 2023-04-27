f46a570a22de1c0e71e15ea56b39c23e1912317b
isaacmg <igodfried@isaac26.com>
2023-04-27 02:07:36 -0300

    r

--- time_series_tables/final_timeserieslab.sql
+++ time_series_tables/final_timeserieslab.sql
@@ -1,4 +1,4 @@
 CREATE TABLE mimic_iv.timeserieslab_final as SELECT * FROM mimic_iv.ld_timeserieslab as t INNER JOIN`gmap-997.mimic_iv.ld_common2` as l 
 on t.labname=l.label
 
--- TIMES OUT THOUGHH :(
+-- TIMES OUT THOUGHH :( ,
