36cf3bea61b3fce712526236cfdd9e81b5cb578c
isaacmg <igodfried@isaac26.com>
2023-04-27 01:54:49 -0300

    EDIT 1

--- time_series_tables/ld_common2.sql
+++ time_series_tables/ld_common2.sql
@@ -1,2 +1,2 @@
 CREATE TABLE mimic_iv.ld_common2 as SELECT * FROM mimic_iv.ld_commonlabs, `gmap-997.mimic_iv.distinct_count` as d1
- WHERE d1.c > 73181 * NUMERIC '0.25'
+ WHERE d1.c > 73181 * NUMERIC '0.26'
