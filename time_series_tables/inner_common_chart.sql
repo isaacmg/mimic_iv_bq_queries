e383448c6c185daae05d025a9ff0b2d68700c6b3
isaacmg <igodfried@isaac26.com>
2023-04-28 21:16:47 -0300

    Revert "stuff r4"
    This reverts commit 76af29238db96a94babcfe4f2516d0d182811352.

--- time_series_tables/inner_common_chart.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE mimic_iv.inner_common_chart as SELECT
-      --  extracting the itemids for all the chartevents that occur within the time bounds for our cohort
-      ch.itemid,
-      la.stay_id
-    FROM
-      mimic_iv.chartevents AS ch
-      INNER JOIN mimic_iv.ld_labels AS la ON la.stay_id = ch.stay_id
-    WHERE ch.valuenum IS NOT NULL
-     AND (UNIX_SECONDS(CAST(CAST(ch.charttime as DATE) AS TIMESTAMP)) - CAST(UNIX_SECONDS(CAST(CAST(la.intime as DATE) AS TIMESTAMP)) as FLOAT64)) / (60 * 60 * 24) BETWEEN -1 AND la.los
